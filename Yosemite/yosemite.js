1579fbe1c8bfb13ab35f45d2e30b79a2a910f40c
Kaleigh Jaeger <jaegerka@dukes.jmu.edu>
2018-11-17 21:53:31 -0500

    Yosemite project Camping and Viewpoints pages completed

--- Yosemite/yosemite.js
+++ Yosemite/yosemite.js
@@ -1,5 +1,5 @@
 var slideIndex = 0;
-window.onload = function showSlides() {
+function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
@@ -10,6 +10,7 @@ window.onload = function showSlides() {
     slides[slideIndex-1].style.display = "block"; 
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
 //OpenWeatherMap API
 const apiUrl = "http://api.openweathermap.org/data/2.5/weather?zip=95389,us&units=imperial&appid=de87fea203d5128c1b10e513a7dad648"
 
@@ -28,7 +29,6 @@ let getWeather = function() {
             var temperature = document.createElement('td');
             var temperatureContent = document.createTextNode('Temperature:' + ' ' + yosemiteWeather.main.temp.toFixed(2) + String.fromCharCode(176) + 'F');
             temperature.appendChild(temperatureContent);
-            console.log(yosemiteWeather.main.temp)
             document.getElementById('weatherTable').appendChild(temperature).appendChild(temperatureIcon)
 
             
@@ -66,13 +66,138 @@ let getWeather = function() {
 
 getWeather();
 
+const NPSAlertURL = 'https://developer.nps.gov/api/v1/alerts?parkCode=yose&api_key=ST20MBOOcrZHBBZXij1k6THaDDQN2JJpDY4eyf6H'
+
+let getNPSalertdata = function() {
+    fetch(NPSAlertURL, {method: "GET", headers:{"Accept":"application/json"}})
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        NPSalert = data;
+        console.log(NPSalert.data[0].title)
+    for (i=0; i < NPSalert.data.length; i++) {
+            var alerts = document.createElement('li');
+            var alertsContent = document.createTextNode(NPSalert.data[i].title);
+            alerts.appendChild(alertsContent);
+            document.getElementById("alertList").appendChild(alerts);
+            console.log(NPSalert.data[i].title);
+    }
+    })
+};
+
+getNPSalertdata();
+
 //NPS API
 const NPSapikey = 'ST20MBOOcrZHBBZXij1k6THaDDQN2JJpDY4eyf6H' 
+const NPSCampingUrl = 'https://developer.nps.gov/api/v1/campgrounds?parkCode=yose&api_key=ST20MBOOcrZHBBZXij1k6THaDDQN2JJpDY4eyf6H'
 
 let getNPSdata = function() {
-    fetch('https://developer.nps.gov/api/v1/campgrounds?parkCode=acad&api_key=ST20MBOOcrZHBBZXij1k6THaDDQN2JJpDY4eyf6H')
+    fetch(NPSCampingUrl, {method: "GET", headers:{"Accept":"application/json"}})
     .then((response) => {
-        return response.json
+        return response.json();
     })
-    .then((data) => {NPSdata = data})
+    .then((data) => {
+    NPSdata = data;
+    console.log(data)
+    for (i=0; i < NPSdata.data.length; i++) {
+        var campsite = document.createElement('li');
+        var campsiteContent = document.createTextNode(NPSdata.data[i].description);
+        campsite.appendChild(campsiteContent);
+        document.getElementById("campingList").appendChild(campsite)
+        var lineBreak = document.createElement('br');
+        document.getElementById("campingList").appendChild(lineBreak);
+    } 
+})
+};
+
+getNPSdata();
+
+
+
+//Zoom Modal 
+
+// Get the modal
+
+window.onload = function() {
+    var modal1 = document.getElementById('glacierPointModal');
+    var modalImg1 = document.getElementById("img01");
+    document.getElementById('glacierPoint').addEventListener("click", function() {
+        modal1.style.display = "block";
+        modalImg1.src = this.src;
+        console.log("modal clicked");
+    })
+
+   
+    var modal2 = document.getElementById('tunnelViewModal');
+    var modalImg2 = document.getElementById("img02");
+    document.getElementById('tunnelView').addEventListener("click", function() {
+        modal2.style.display = "block";
+        modalImg2.src = this.src;
+        console.log("modal clicked");
+    })
+
+    var modal3 = document.getElementById('olmsteadPointModal');
+    var modalImg3 = document.getElementById("img03");
+    document.getElementById('olmsteadPoint').addEventListener("click", function() {
+        modal3.style.display = "block";
+        modalImg3.src = this.src;
+        console.log("modal clicked");
+    })
+
+    var modal4 = document.getElementById('elCapitanMeadowModal');
+    var modalImg4 = document.getElementById("img04");
+    document.getElementById('elCapitanMeadow').addEventListener("click", function() {
+        modal4.style.display = "block";
+        modalImg4.src = this.src;
+        console.log("modal clicked");
+    })
+
+    var modal5 = document.getElementById('valleyViewModal');
+    var modalImg5 = document.getElementById("img05");
+    document.getElementById('valleyView').addEventListener("click", function() {
+        modal5.style.display = "block";
+        modalImg5.src = this.src;
+        console.log("modal clicked");
+    })
+
+    var modal6 = document.getElementById('lowerYosemiteFallsModal');
+    var modalImg6 = document.getElementById("img06");
+    document.getElementById('lowerYosemiteFalls').addEventListener("click", function() {
+        modal6.style.display = "block";
+        modalImg6.src = this.src;
+        console.log("modal clicked");
+    })
+
+    
+    // When the user clicks on <span> (x), close the modal
+    document.getElementById('close1').addEventListener("click", function() { 
+        modal1.style.display = "none";
+    })
+
+    document.getElementById('close2').addEventListener("click", function() { 
+        modal2.style.display = "none";
+    })
+    
+    document.getElementById('close3').addEventListener("click", function() { 
+        modal3.style.display = "none";
+    })
+
+    document.getElementById('close4').addEventListener("click", function() { 
+        modal4.style.display = "none";
+    })
+
+    document.getElementById('close5').addEventListener("click", function() { 
+        modal5.style.display = "none";
+    })
+
+    document.getElementById('close6').addEventListener("click", function() { 
+        modal6.style.display = "none";
+    })
+
+    
+
+
+
+
 }
